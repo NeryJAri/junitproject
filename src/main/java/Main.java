37df863d90ad64b3c902c9b48a8715811fc53ed6
NeryJAri <neryjarizandieta@gmail.com>
2024-10-23 01:19:30 -0600

    Se agrega codigo al Main una clase nueva para Fizz Buzz y un Test para FizzBuzz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String [] args){
         double salary = 1000;
         System.out.println(MoneyUtil.format(salary));
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> result = fizzBuzz.getNumbers();
+        for (String number : result) {
+            System.out.println(number);
+        }
+
     }
 }
